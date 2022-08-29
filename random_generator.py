4477c7f1023ef812476dc7493f3747aa5c9f103c
mks2122 <68767491+mks2122@users.noreply.github.com>
2022-08-29 23:04:27 +0530

    Update random_generator.py

--- random_generator.py
+++ random_generator.py
@@ -1,16 +1,16 @@
 import random
-
+rom PIL import Image,ImageDraw,ImageFont
 
 obj=open('inspire.txt','rb')
 handle=obj.read()
 
 words=handle.splitlines()
 
-
-obj.close()
 quote =str(random.choice(words),'utf-8')
+obj.close()
 
-from PIL import Image,ImageDraw,ImageFont
+
+
 myf=ImageFont.truetype('arial',size=22)
 image = Image.new(mode='RGB',size=(1920,1080),color='black')
 t= ImageDraw.Draw(image)
